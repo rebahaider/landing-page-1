d3c8f8d685e9d8785970ccadbefe69ee1a531f74
rebahaider <rebahaiderema@gmail.com>
2024-07-17 02:03:53 +0600

    added popup form

--- main.js
+++ main.js
@@ -1,9 +1,30 @@
 // video popup
 function openVideo() {
-    document.getElementById("video-popup").style.display="block";
+    document.getElementById("video-popup").style.display = "block";
 }
 function closeVideo() {
     document.getElementById("video-popup").style.display = "none";
     var iframe = document.getElementById("video-frame");
     iframe.src = iframe.src; // Stop the video
 }
+
+// modal 1
+document.addEventListener("DOMContentLoaded", function() {
+    var modal = document.getElementById("myModal");
+    var btn = document.getElementById("train-btn");
+    var span = document.getElementsByClassName("close")[0];
+
+    btn.onclick = function() {
+        modal.style.display = "block";
+    }
+
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+});
